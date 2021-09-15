f5b78e2837e144e0c12246c6059495dee01c4323
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-09-15 11:28:07 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,5 +1,22 @@
 var rndnum;
+var theirQ;
 
 function press(){
     rndnum = Math.random();
+    rndnum = Math.floor(rndnum * 10);
+    theirQ = "> " + document.getElementById("question").value;
+    document.getElementById("question").value = "";
+    document.getElementById("answer").innerHTML = theirQ + "</br>" + getWords(rndnum);
 }//end press
+
+//This function will return a sentence
+// please customize it and add your own.
+//please have at least 10 responses.
+function getWords(number){
+    switch(number){
+      case 0:
+        return("Hell No");
+      case 1:
+        return("I think so yes.");
+    }
+}//end getWords
